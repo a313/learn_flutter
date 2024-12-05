5e821fb630e0e7363310eb821366e3fa4210764a
tungnt <nguyenthanhtung0207@gmail.com>
2024-12-05 23:37:52 +0700

    update url open id

--- lib/presentation/authen/open_id/open_id_controller.dart
+++ lib/presentation/authen/open_id/open_id_controller.dart
@@ -1,23 +1,34 @@
+import 'dart:developer';
+
 import 'package:get/get.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:widgets/routes/routes.dart';
 
 class OpenIdController extends GetxController {
   late final WebViewController webController;
+
+  final clientId = '8B08047B-4C10-4A76-AA5E-17412D7FE416';
+  final redirectUri = 'https://test.consio.no/OpenIdtest/';
+  final nonce = 'AqKGYZXbAhLijdXFtNVF6Hdd8us%3d';
   @override
   void onInit() {
     super.onInit();
-    final initUri = Uri.parse(
-        'https://test.consio.no/OpenIdserver/connect/authorize?client_id=3609848B-B4D3-4DD0-8E65-83276315688D&redirect_uri=https%3a%2f%2ftest.consio.no%2fInternTestLoginID%2fauthentication%2flogin-callback&response_type=id_token+token&grant_type=implicit&scope=openid+profile&nonce=v0cvCsMuen3itBy68ODV5TxFHH8%3d');
+    //10107561
+    //tamtest
+    final urlStr =
+        'https://test.consio.no/authminside/connect/authorize?client_id=$clientId&redirect_uri=$redirectUri&response_type=id_token+token&grant_type=implicit&scope=openid+profile&nonce=$nonce';
+
+    final initUri = Uri.parse(urlStr);
+
+    const callbackUrl = 'https://test.consio.no/OpenIdtest';
 
     webController = WebViewController();
     webController.setNavigationDelegate(
       NavigationDelegate(onNavigationRequest: (NavigationRequest request) {
-        if (request.url.startsWith(
-            'https://test.consio.no/InternTestLoginID/authentication/login-callback')) {
+        if (request.url.startsWith(callbackUrl)) {
           final uri = Uri.parse(request.url);
           final fragment = uri.fragment;
-          final token = fragment.replaceAll('access_token=', '').split('&')[0];
+          final token = extractBetween(fragment, 'access_token=', '&');
           loginSuccess(token);
           return NavigationDecision.prevent;
         }
@@ -27,9 +38,18 @@ class OpenIdController extends GetxController {
     webController.loadRequest(initUri);
   }
 
+  String extractBetween(String input, String startKey, String endKey) {
+    int startIndex = input.indexOf(startKey);
+    if (startIndex == -1) return '';
+    int endIndex = input.indexOf(endKey, startIndex + startKey.length);
+    if (endIndex == -1) return '';
+    return input.substring(startIndex + startKey.length, endIndex);
+  }
+
   void loginSuccess(String token) {
     //TODO
     // Store token
+    log(token, name: 'Access Token');
     Get.offAllNamed(Routes.home);
   }
 }
